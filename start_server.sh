47e8a85d1f1ca6785948505f13623f9b1e9ebc22
435886030 <435886030@qq.com>
2016-01-21 15:35:08 +0800

    Update start_server.sh
    提交demo的服务器可执行文件搜索目录

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,7 @@
 
 export KBE_ROOT=$(cd ../; pwd)
 export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$KBE_ROOT/kbengine_demos_assets:$KBE_ROOT/kbengine_demos_assets/res/:$KBE_ROOT/kbengine_demos_assets/scripts/"
-export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
+export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/Hybrid64/:$KBE_ROOT/kbe/bin/Hybrid/"
 
 echo KBE_ROOT = \"${KBE_ROOT}\"
 echo KBE_RES_PATH = \"${KBE_RES_PATH}\"
